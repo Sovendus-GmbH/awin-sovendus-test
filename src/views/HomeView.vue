8fa3cdddeee13e844f7b74024655ebeb22caa631
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2024-07-04 10:14:03 +0200

    add master tag to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts">
-import { SovendusLandingPage } from 'sovendus-voucher-network-and-checkout-benefits-vue';
+<script setup>
+import { SovendusLandingPage } from 'sovendus-voucher-network-and-checkout-benefits-vue'
 import TheWelcome from '../components/TheWelcome.vue'
+const master = document.createElement('script')
+master.src = 'https://www.dwin1.com/14411.js'
+master.type = 'text/javascript'
+master.defer = 'defer'
+document.body.appendChild(master)
 </script>
 
 <template>
